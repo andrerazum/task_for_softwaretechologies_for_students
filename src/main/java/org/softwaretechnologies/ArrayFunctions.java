97e7367bf005f39dc757cf0cd064f1779894726d
aarazumovskiy <r@m.ru>
2025-11-07 09:41:59 +0300

    ZZZZ

--- src/main/java/org/softwaretechnologies/ArrayFunctions.java
+++ src/main/java/org/softwaretechnologies/ArrayFunctions.java
@@ -10,20 +10,36 @@ public final class ArrayFunctions {
      * @param array массив, который будет перевернут.
      */
     public static void reverse(int[] array) {
-        // TODO: реализуйте вышеуказанную функцию
+        int tmp;
+        for (int i = 0; i < (array.length)/2; i++) {
+            tmp = array[i];
+            array[i] = array[array.length - i - 1];
+            array[array.length - i - 1] = tmp;
+        }
     }
 
     /**
      * Функция, заменяющая строки матрицы на столбцы матрицы. Пример:
-       1  2  3     1  4  7
-       4  5  6     2  5  8
-       7  8  9     3  6  9
+     1  2  3     1  4  7
+     4  5  6     2  5  8
+     7  8  9     3  6  9
      * Функция работает только с квадратными матрицами.
      * Если матрица не квадратная, то выведете на экран сообщение:
-       Матрица не квадратная
+     Матрица не квадратная
      * @param matrix матрица, в которой столбцы будут заменены на строки.
      */
     public static void rotateMatrix(int[][] matrix) {
-        // TODO: реализуйте вышеуказанную функцию
+        int tmp;
+        if(matrix.length != matrix[0].length){
+            System.out.println("Матрица не квадратная");
+            return;
+        }
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = i; j < matrix[0].length; j++) {
+                tmp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = tmp;
+            }
+        }
     }
 }
